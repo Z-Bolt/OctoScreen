9322f978dee983d0c7c42e6dd2dced6020223a87
Máximo Cuadros <mcuadros@gmail.com>
2017-11-26 20:49:00 +0100

    octoprint: new OctoPrint REST API client
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

---
+++ octoprint/printer.go
@@ -0,0 +1,62 @@
+package octoprint
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+)
+
+type Printer struct {
+	key      string
+	endpoint string
+	c        *http.Client
+}
+
+func NewPrinter(endpoint, key string) *Printer {
+	return &Printer{
+		endpoint: endpoint,
+		key:      key,
+		c:        &http.Client{},
+	}
+}
+
+func (p *Printer) doRequest(method, target string, body io.Reader) ([]byte, error) {
+	req, err := http.NewRequest(method, joinURL(p.endpoint, target), body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-Api-Key", p.key)
+
+	resp, err := p.c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.handleResponse(resp)
+}
+
+func (c *Printer) handleResponse(r *http.Response) ([]byte, error) {
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if r.StatusCode >= 200 && r.StatusCode <= 209 {
+		return body, nil
+	}
+
+	fmt.Println(body)
+	return nil, nil
+}
+
+func joinURL(base, uri string) string {
+	u, _ := url.Parse(base)
+	u.Path = path.Join(u.Path, uri)
+	return u.String()
+}
---
+++ octoprint/printhead.go
@@ -0,0 +1,48 @@
+package octoprint
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+)
+
+// JogCommand jogs the print head (relatively) by a defined amount in one or
+// more axes.
+type JogCommand struct {
+	// X is the amount distance to travel in mm or coordinate to jog print head
+	// on x axis.
+	X int `json:"x,omitempty"`
+	// Y is the amount distance to travel in mm or coordinate to jog print head
+	// on y axis.
+	Y int `json:"y,omitempty"`
+	// Z is the amount distance to travel in mm.or coordinate to jog print head
+	// on x axis.
+	Z int `json:"z,omitempty"`
+	// Absolute is whether to move relative to current position (provided axes
+	// values are relative amounts) or to absolute position (provided axes
+	// values are coordinates)
+	Absolute bool `json:"absolute"`
+	// Speed at which to move in mm/s. If not provided, minimum speed for all
+	// selected axes from printer profile will be used.
+	Speed int `json:"speed,omitempty"`
+}
+
+func (cmd *JogCommand) Do(p *Printer) error {
+	b := bytes.NewBuffer(nil)
+	if err := cmd.encode(b); err != nil {
+		return err
+	}
+
+	_, err := p.doRequest("POST", "/api/printer/printhead", b)
+	return err
+}
+
+func (cmd *JogCommand) encode(w io.Writer) error {
+	return json.NewEncoder(w).Encode(struct {
+		Command string `json:"command"`
+		JogCommand
+	}{
+		Command:    "jog",
+		JogCommand: *cmd,
+	})
+}
