72c6a215eb1bfcb56f669207cca6f1e12a81d4c5
Máximo Cuadros <mcuadros@gmail.com>
2017-11-27 19:05:23 +0100

    octoprint: JobCommand implementation
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

--- octoprint/common.go
+++ octoprint/common.go
@@ -13,6 +13,57 @@ const (
 	ZAxis Axis = "z"
 )
 
+// JobInformation contains information regarding the target of the current job.
+type JobInformation struct {
+	// File is the file that is the target of the current print job.
+	File FileInformation `json:"file"`
+	// EstimatedPrintTime is the estimated print time for the file, in seconds.
+	EstimatedPrintTime int `json:"estimatedPrintTime"`
+	// LastPrintTime is the print time of the last print of the file, in seconds.
+	LastPrintTime int `json:"lastPrintTime"`
+	// Filament contains Information regarding the estimated filament
+	// usage of the print job.
+	Filament struct {
+		// Length of filament used, in mm
+		Length int `json:"length"`
+		// Volume of filament used, in cm³
+		Volume float64 `json:"volume"`
+	} `json:"filament"`
+	FilePosition int `json:"filepos"`
+}
+
+// FileInformation contains information regarding a file.
+type FileInformation struct {
+	// Name is name of the file without path. E.g. “file.gco” for a file
+	// “file.gco” located anywhere in the file system.
+	Name string `json:"name"`
+	// Path is the path to the file within the location. E.g.
+	//“folder/subfolder/file.gco” for a file “file.gco” located within “folder”
+	// and “subfolder” relative to the root of the location.
+	Path string `json:"path"`
+	// Type of file. model or machinecode. Or folder if it’s a folder, in
+	// which case the children node will be populated.
+	Type string `json:"type"`
+	// TypePath path to type of file in extension tree. E.g. `["model", "stl"]`
+	// for .stl files, or `["machinecode", "gcode"]` for .gcode files.
+	// `["folder"]` for folders.
+	TypePath string `json:"typePath"`
+}
+
+// ProgressInformation contains information regarding the progress of the
+// current print job.
+type ProgressInformation struct {
+	// Completion percentage of completion of the current print job.
+	Completion float64 `json:"completion"`
+	// FilePosition current position in the file being printed, in bytes
+	// from the beginning.
+	FilePosition int `json:"filepos"`
+	// PrintTime is time already spent printing, in seconds
+	PrintTime int `json:"printTime"`
+	// PrintTimeLeft is estimate of time left to print, in seconds
+	PrintTimeLeft int `json:"printTimeLeft"`
+}
+
 // CurrentState of a tool.
 type CurrentState struct {
 	State
--- octoprint/job.go
+++ octoprint/job.go
@@ -6,17 +6,25 @@ import (
 
 const JobTool = "/api/job"
 
-// StateCommand retrieves the current state of the printer.
+// JobCommand retrieve information about the current job (if there is one).
 type JobCommand struct{}
 
+// JobResponse is the response from a job command.
+type JobResponse struct {
+	// Job contains information regarding the target of the current print job.
+	Job JobInformation `json:"job"`
+	// Progress contains information regarding the progress of the current job.
+	Progress ProgressInformation `json:"progress"`
+}
+
 // Do sends an API request and returns the API response.
-func (cmd *JobCommand) Do(c *Client) (*StateResponse, error) {
+func (cmd *JobCommand) Do(c *Client) (*JobResponse, error) {
 	b, err := c.doRequest("GET", JobTool, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	r := &StateResponse{}
+	r := &JobResponse{}
 	if err := json.Unmarshal(b, r); err != nil {
 		return nil, err
 	}
