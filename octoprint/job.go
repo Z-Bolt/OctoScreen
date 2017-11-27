9087269dd53cad7f78993eb9fb5475a1cb0ce1e4
Máximo Cuadros <mcuadros@gmail.com>
2017-11-27 20:32:43 +0100

    octoprint: Job, start, stop, restart and pause action implementation
    Signed-off-by: Máximo Cuadros <mcuadros@gmail.com>

--- octoprint/job.go
+++ octoprint/job.go
@@ -1,7 +1,9 @@
 package octoprint
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 )
 
 const JobTool = "/api/job"
@@ -22,4 +24,96 @@ func (cmd *JobCommand) Do(c *Client) (*JobResponse, error) {
 	}
 
 	return r, err
+}
+
+// StartCommand starts the print of the currently selected file.
+type StartCommand struct{}
+
+// Do sends an API request and returns an error if any.
+func (cmd *TargetCommand) Do(c *Client) error {
+	payload := map[string]string{"command": "start"}
+
+	b := bytes.NewBuffer(nil)
+	if err := json.NewEncoder(b).Encode(payload); err != nil {
+		return err
+	}
+
+	_, err := c.doRequest("POST", URITool, b)
+	return err
+}
+
+// CancelCommand cancels the current print job.
+type CancelCommand struct{}
+
+// Do sends an API request and returns an error if any.
+func (cmd *CancelCommand) Do(c *Client) error {
+	payload := map[string]string{"command": "cancel"}
+
+	b := bytes.NewBuffer(nil)
+	if err := json.NewEncoder(b).Encode(payload); err != nil {
+		return err
+	}
+
+	_, err := c.doRequest("POST", URITool, b)
+	return err
+}
+
+// RestartCommand restart the print of the currently selected file from the
+// beginning. There must be an active print job for this to work and the print
+// job must currently be paused
+type RestartCommand struct{}
+
+// Do sends an API request and returns an error if any.
+func (cmd *RestartCommand) Do(c *Client) error {
+	payload := map[string]string{"command": "restart"}
+
+	b := bytes.NewBuffer(nil)
+	if err := json.NewEncoder(b).Encode(payload); err != nil {
+		return err
+	}
+
+	_, err := c.doRequest("POST", URITool, b)
+	return err
+}
+
+type PauseAction string
+
+const (
+	// Pause the current job if it’s printing, does nothing if it’s already paused.
+	Pause PauseAction = "pause"
+	// Resume the current job if it’s paused, does nothing if it’s printing.
+	Resume PauseAction = "resume"
+	// Toggle the pause state of the job, pausing it if it’s printing and
+	// resuming it if it’s currently paused.
+	Toggle PauseAction = "toggle"
+)
+
+// PauseCommand pauses/resumes/toggles the current print job.
+type PauseCommand struct {
+	// Action specifies which action to take.
+	// In order to stay backwards compatible to earlier iterations of this API,
+	// the default action to take if no action parameter is supplied is to
+	// toggle the print job status.
+	Action PauseAction `json:"action"`
+}
+
+// Do sends an API request and returns an error if any.
+func (cmd *PauseCommand) Do(c *Client) error {
+	b := bytes.NewBuffer(nil)
+	if err := cmd.encode(b); err != nil {
+		return err
+	}
+
+	_, err := c.doRequest("POST", URITool, b)
+	return err
+}
+
+func (cmd *PauseCommand) encode(w io.Writer) error {
+	return json.NewEncoder(w).Encode(struct {
+		Command string `json:"command"`
+		PauseCommand
+	}{
+		Command:      "pause",
+		PauseCommand: *cmd,
+	})
 }
