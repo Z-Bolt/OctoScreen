e87f6d0b34403bc91b368df1372f69d178eddc22
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2020-08-08 11:37:57 -0700

    Added fatal_error_window.go to display fatal startup messages

---
+++ ui/fatal_error_window.go
@@ -0,0 +1,32 @@
+package ui
+
+import (
+	"github.com/gotk3/gotk3/gtk"
+)
+
+// CreateFatalErrorWindow - (Captain obvious says) Creates a window to display fatal errors.
+func CreateFatalErrorWindow(message string, description string) *gtk.Window {
+	window, error := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if error != nil {
+		Logger.Fatalln("Unable to create window: ", error)
+	}
+
+	window.SetTitle("Fatal Error")
+	window.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	window.SetDefaultSize(800, 480)
+
+	// Create a new label widget to show in the window.
+	label, error := gtk.LabelNew("\n    " + message + "\n    " + description)
+	if error != nil {
+		Logger.Fatalln("Unable to create label: ", error)
+	}
+
+	label.SetHAlign(gtk.ALIGN_START)
+	label.SetVAlign(gtk.ALIGN_START)
+	window.Add(label)
+
+	return window
+}
