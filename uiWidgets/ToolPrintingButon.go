07a87ee50b408f17ff93ba12fb7049cb45f399e0
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2020-11-15 17:29:35 -0800

    added ToolPrintingButon.go

---
+++ uiWidgets/ToolPrintingButon.go
@@ -0,0 +1,25 @@
+package uiWidgets
+
+import (
+	"github.com/gotk3/gotk3/gtk"
+	// "github.com/mcuadros/go-octoprint"
+	"github.com/Z-Bolt/OctoScreen/utils"
+)
+
+type ToolPrintingButton struct {
+	*gtk.Button
+}
+
+func CreateToolPrintingButton(
+	index			int,
+) *ToolPrintingButton {
+	imageFileName := ToolImageFileName(index)
+	instance := &ToolPrintingButton{
+		Button:  utils.MustButtonImage("", imageFileName, nil),
+	}
+
+	ctx, _ := instance.GetStyleContext()
+	ctx.AddClass("printing-state")
+
+	return instance
+}
