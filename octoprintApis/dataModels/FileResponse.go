b6734629a80d4bc8c57f19c13d48d3ca23ebe600
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2021-02-27 11:42:26 -0800

    updated comment

--- octoprintApis/dataModels/FileResponse.go
+++ octoprintApis/dataModels/FileResponse.go
@@ -75,7 +75,8 @@ type FileResponse struct {
 
 
 	// Relative path to the preview thumbnail image (if it exists)
-	// The PrusaSlicer Thumbnails plug-in is required or this.
+	// The PrusaSlicer Thumbnails plug-in or the Cura Thumbnails plug-in
+	// is required for this.
 	Thumbnail string `json:"thumbnail"`
 }
 
