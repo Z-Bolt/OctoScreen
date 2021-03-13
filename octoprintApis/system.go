513dacbc13dfa82f541a29412e59d0986387a5d3
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2021-03-13 09:05:15 -0800

    fixed bug in SystemCommandsRequest.Do() where Unmarshal() was being passed a string and not a *string

--- octoprintApis/system.go
+++ octoprintApis/system.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/Z-Bolt/OctoScreen/logger"
 	"github.com/Z-Bolt/OctoScreen/octoprintApis/dataModels"
 )
 
@@ -22,33 +23,37 @@ type SystemCommandsRequest struct{}
 
 // Do sends an API request and returns the API response.
 func (cmd *SystemCommandsRequest) Do(c *Client) (*dataModels.SystemCommandsResponse, error) {
-	b, err := c.doJsonRequest("GET", SystemCommandsApiUri, nil, nil)
+	bytes, err := c.doJsonRequest("GET", SystemCommandsApiUri, nil, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO: there are 2 warnings here... the 2nd parameter into Unmarshal()
-	// is supposed to be a pointer.  Change x.Confirm to &x.Confirm ?
-	// need to verify
-
-	r := &dataModels.SystemCommandsResponse{}
-	if err := json.Unmarshal(b, r); err != nil {
+	response := &dataModels.SystemCommandsResponse{}
+	if err := json.Unmarshal(bytes, response); err != nil {
 		return nil, err
 	}
-	for i := range r.Core {
-		x := r.Core[i]
-		if err2 := json.Unmarshal(x.RawConfirm, x.Confirm); err2 != nil {
-		    x.Confirm = ""
-		}
-	}
-	for i := range r.Custom {
-		x := r.Custom[i]
-		if err2 := json.Unmarshal(x.RawConfirm, x.Confirm); err2 != nil {
-		    x.Confirm = ""
+
+	for i := range response.Core {
+		commandDefinition := response.Core[i]
+		err = json.Unmarshal(commandDefinition.RawConfirm, &commandDefinition.Confirm)
+		if err != nil {
+			logger.LogError("SystemCommandsRequest.Do()", "json.Unmarshal(Core)", err)
+			commandDefinition.Confirm = ""
+			return nil, err
 		}
 	}
 
-	return r, err
+	for i := range response.Custom {
+		commandDefinition := response.Custom[i]
+		err = json.Unmarshal(commandDefinition.RawConfirm, &commandDefinition.Confirm)
+		if err != nil {
+			logger.LogError("SystemCommandsRequest.Do()", "json.Unmarshal(Custom)", err)
+			commandDefinition.Confirm = ""
+			return nil, err
+		}
+	}
+
+	return response, err
 }
 
 // SystemExecuteCommandRequest retrieves all configured system commands.
