fc972a63af3d879b4699c786cd1d30dd4a2d7328
Michael J Mulligan <mike@belineperspectives.com>
2021-03-15 12:29:58 +0000

    Just trying to figure out where I am...

--- ui/FilesPanel.go
+++ ui/FilesPanel.go
@@ -34,7 +34,9 @@ func FilesPanel(
 	ui					*UI,
 	parentPanel			interfaces.IPanel,
 ) *filesPanel {
+	logger.Info("Begin filesPanel()")
 	if filesPanelInstance == nil {
+		logger.Info("Making New filesPanel")
 		locationHistory := utils.LocationHistory {
 			Locations: []dataModels.Location{},
 		}
@@ -47,6 +49,7 @@ func FilesPanel(
 		filesPanelInstance = instance
 	}
 
+	logger.Info("End filesPanel()")
 	return filesPanelInstance
 }
 
