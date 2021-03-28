f1a3ad23c43bb8b78e42ebe6e943af6bd1fb8b49
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2021-03-28 11:25:35 -0700

    changed the trottle from 5 sec to 2 sec, and updated the comment

--- octoprintApis/OctoScreenSettingsRequest.go
+++ octoprintApis/OctoScreenSettingsRequest.go
@@ -17,11 +17,11 @@ type OctoScreenSettingsRequest struct {
 }
 
 func (this *OctoScreenSettingsRequest) Do(client *Client, uiState string) (*dataModels.OctoScreenSettingsResponse, error) {
-	// Pause for 5 seconds here.  This is in response to the "OctoScreen 2.7.0 doesn't recognize that OctoScreen plugin 2.6.0 is installed"
-	// bug (see https://github.com/Z-Bolt/OctoScreen/issues/275).  I was able to repro the bug sometimes, yet other times it worked fine.
+	// Pause for 2 seconds here.  This is in response to the "OctoScreen 2.7.0 doesn't recognize that OctoScreen plugin 2.6.0 is installed"
+	// bug (see https://github.com/Z-Bolt/OctoScreen/issues/275).  I was able to repro the bug sometimes, yet other times this worked fine.
 	// I examined the logs and they were exactly the same (up to the error, which was "HTTP/1.x transport connection broken: malformed MIME
-	// header line: Not found").  Very weird.
-	time.Sleep(time.Second * 5)
+	// header line: Not found").  This might not be an issue after the state machine rewrite in 2.8.
+	time.Sleep(time.Second * 2)
 
 	target := fmt.Sprintf("%s?command=get_settings", PluginZBoltOctoScreenApiUri)
 	bytes, err := client.doJsonRequest("GET", target, nil, ConnectionErrors)
