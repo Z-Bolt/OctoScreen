2fad643c00efc35d0c8c086bac522974c6d3e547
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2021-03-29 22:54:19 -0700

    reverted call to Sleep()

--- octoprintApis/OctoScreenSettingsRequest.go
+++ octoprintApis/OctoScreenSettingsRequest.go
@@ -3,7 +3,6 @@ package octoprintApis
 import (
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"github.com/Z-Bolt/OctoScreen/octoprintApis/dataModels"
 )
@@ -17,12 +16,6 @@ type OctoScreenSettingsRequest struct {
 }
 
 func (this *OctoScreenSettingsRequest) Do(client *Client, uiState string) (*dataModels.OctoScreenSettingsResponse, error) {
-	// Pause for 2 seconds here.  This is in response to the "OctoScreen 2.7.0 doesn't recognize that OctoScreen plugin 2.6.0 is installed"
-	// bug (see https://github.com/Z-Bolt/OctoScreen/issues/275).  I was able to repro the bug sometimes, yet other times this worked fine.
-	// I examined the logs and they were exactly the same (up to the error, which was "HTTP/1.x transport connection broken: malformed MIME
-	// header line: Not found").  This might not be an issue after the state machine rewrite in 2.8.
-	time.Sleep(time.Second * 2)
-
 	target := fmt.Sprintf("%s?command=get_settings", PluginZBoltOctoScreenApiUri)
 	bytes, err := client.doJsonRequest("GET", target, nil, ConnectionErrors)
 	if err != nil {
