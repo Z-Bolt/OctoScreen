f758435125677f332d0c458860877959e7adf402
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2021-04-03 15:46:38 -0700

    added code to force the cursor to be shown

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/sirupsen/logrus"
 
@@ -122,6 +123,8 @@ func main() {
 	settings.SetProperty("gtk-application-prefer-dark-theme", true)
 
 	utils.DumpEnvironmentVariables()
+
+	setCursor()
 
 	if initSucceeded != true {
 		// readConfig() logs any errors it encounters.  Don't display
@@ -367,4 +370,48 @@ func getSize() (width int, height int, err error) {
 
 	logger.TraceLeave("main.getSize()")
 	return
+}
+
+func setCursor() {
+	/*
+	if todo: test for new env setting to be defined and not be false {
+		return
+	}
+	*/
+
+	window, err := getRootWindow()
+	if err != nil {
+		return
+	}
+
+	cursor, err := getDefaultCursor()
+	if err != nil {
+		return
+	}
+
+	window.SetCursor(cursor)
+}
+
+func getRootWindow() (*gdk.Window, error) {
+	screen, err := gdk.ScreenGetDefault()
+	if err != nil {
+		return nil, err
+	}
+	
+	window, err := screen.GetRootWindow()
+
+	return window, err
+}
+
+func getDefaultCursor() (*gdk.Cursor, error) {
+	display, err := gdk.DisplayGetDefault()
+	if err != nil {
+		return nil, error
+	}
+
+	// Examples of the different cursors can be found at
+	// https://developer.gnome.org/gdk3/stable/gdk3-Cursors.html#gdk-cursor-new-from-name
+	cursor, err := gdk.CursorNewFromName(display, "default")
+
+	return cursor, err
 }
