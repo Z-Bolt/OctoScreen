30729df2097ecd021cd9dda47d8e4142fba8ac2b
JeffB42 <10328858+JeffB42@users.noreply.github.com>
2023-07-09 12:22:38 -0700

    added RowIndex to ListBoxRow

--- uiWidgets/ListBoxRow.go
+++ uiWidgets/ListBoxRow.go
@@ -7,22 +7,49 @@ import (
 )
 
 type ListBoxRow struct {
-	*gtk.ListBoxRow
-	rowContentsBox		*gtk.Box
+	gtk.ListBoxRow
+
+	ContentsBox			*gtk.Box
+	RowIndex			int
 }
 
 func CreateListBoxRow(
-	index				int,
+	rowIndex			int,
 	padding				int,
 ) *ListBoxRow {
-	base := createListBoxRow(index)
+	/*
+		Object hierarchy:
+		(for a static, non-clickable list item)
 
-	rowContentsBox := createRowContentsBox(padding)
-	base.Add(rowContentsBox)
+		ScrollableListBox (ScrolledWindow + ListBox)
+			ListBoxRow
+				ContentsBox (to layout the objects)
+
+
+		Object hierarchy:
+		(for a clickable list item/button)
+
+		ScrollableListBox (ScrolledWindow + ListBox)
+			ClickableListBoxRow
+				listItemButton (to handle to click for the entire item amd all of the child controls)
+					ContentsBox (to layout the objects)
+	*/
+
+	base := createListBoxRow(rowIndex)
+	// ctx1, _ := base.GetStyleContext()
+	// ctx1.AddClass("blue-background")
+
+	contentsBox := createContentsBox(padding) // jab rename
+	// ctx2, _ := contentsBox.GetStyleContext()
+	// ctx2.AddClass("cyan-background-JAB-NO-USE")
+
+	base.Add(contentsBox)
+
 
 	instance := &ListBoxRow {
-		ListBoxRow:				base,
-		rowContentsBox:			rowContentsBox,
+		ListBoxRow:				*base,
+		ContentsBox:			contentsBox,
+		RowIndex:				rowIndex,
 	}
 
 	return instance
@@ -32,25 +59,24 @@ func CreateListBoxRow(
 // A uiWidgets.ListBoxRow will often have many children in it,
 // so a contents box is added to the gtk.ListBoxRow, and when a
 // child widget is added to uiWidgets.ListBoxRow, it will actually
-// be added to rowContentsBox.
+// be added to contentsBox.
 //
-// 		Object hierarchy:
-//			gtk.ListBoxRow (base)
-//				gtk.Box (rowContentsBox)
+// 	Object hierarchy:
+//		gtk.ListBoxRow (base)
+//			gtk.Box (ContentsBox)
 func (this *ListBoxRow) Add(widget gtk.IWidget) {
-	this.rowContentsBox.Add(widget)
+	this.ContentsBox.Add(widget)
 }
-
 
 
 // Internal functions
 func createListBoxRow(
-	index			int,
+	rowIndex		int,
 ) *gtk.ListBoxRow {
 	listBoxRow, _ := gtk.ListBoxRowNew()
 	listBoxRowStyleContext, _ := listBoxRow.GetStyleContext()
 	listBoxRowStyleContext.AddClass("list-box-row")
-	if index % 2 != 0 {
+	if rowIndex % 2 != 0 {
 		listBoxRowStyleContext.AddClass("list-item-nth-child-even-background-color")
 	} else {
 		listBoxRowStyleContext.AddClass("list-item-nth-child-odd-background-color")
@@ -59,14 +85,14 @@ func createListBoxRow(
 	return listBoxRow
 }
 
-func createRowContentsBox(padding int) *gtk.Box {
-	rowContentsBox := utils.MustBox(gtk.ORIENTATION_HORIZONTAL, 0)
-	rowContentsBox.SetMarginTop(padding)
-	rowContentsBox.SetMarginBottom(padding)
-	rowContentsBox.SetMarginStart(padding)
-	rowContentsBox.SetMarginEnd(padding)
-	rowContentsBox.SetHAlign(gtk.ALIGN_START)
-	// base.SetHExpand(true) // TODO: should this be set?
+func createContentsBox(padding int) *gtk.Box {
+	contentsBox := utils.MustBox(gtk.ORIENTATION_HORIZONTAL, 0)
+	contentsBox.SetMarginTop(padding)
+	contentsBox.SetMarginBottom(padding)
+	contentsBox.SetMarginStart(padding)
+	contentsBox.SetMarginEnd(padding)
+	contentsBox.SetHAlign(gtk.ALIGN_START)
+	// contentsBox.SetHExpand(true) // TODO: should this be set?
 
-	return rowContentsBox
+	return contentsBox
 }
